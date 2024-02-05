88ac43d2d1a5fa88edc49b57ca1e461de89513f2
ashif29 <asifcr75@gmail.com>
2024-02-06 03:01:46 +0600

    Add project files.

---
+++ QueryFlow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QueryFlow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
