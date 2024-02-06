11cb0b2900f2a80eb54ecaf9066f4ff8ed7bbf4c
ashif29 <asifcr75@gmail.com>
2024-02-06 10:33:16 +0600

    Added Question and Comment model inside Domain layer and installed NuGet packages for infrastructure layer and Presentation Layer.

---
+++ QueryFlow.Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryFlow.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string CommentContent { get; set; }
+        public string CommentedBy { get; set; }
+        public Comment Parent { get; set; }
+        public Question ParentQuestion { get; set; }
+    }
+}
---
+++ QueryFlow.Domain/Entities/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QueryFlow.Domain.Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string QuestionContent { get; set; }
+    }
+}
